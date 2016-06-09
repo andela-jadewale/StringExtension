5fbe41d6bb5cda44eaf54fb1a4302689d4d94609
Jolaade Adewale <jolaade.adewale@andela.com>
2016-06-09 18:19:07 +0100

    implement toLower function

--- src/StringExtension.js
+++ src/StringExtension.js
@@ -7,6 +7,14 @@ String.prototype.toUpper = function () {
   return validData(this) ? this.replace(/[a-z]/g, upperCase) : '';
 };
 
+String.prototype.toLower = function () {
+  return validData(this) ? this.replace(/([A-Z])/g, lowerCase) : '';
+};
+
+function lowerCase(word) {
+  return String.fromCharCode(word.charCodeAt() + 32);
+}
+
 function upperCase(word) {
   return String.fromCharCode(word.charCodeAt() - 32);
 }
