4ca602c040d5d6db51e40ab059e2a79ed2ecc8dd
Jolaade Adewale <jolaade.adewale@andela.com>
2016-06-09 18:17:29 +0100

    implement toUpper function

--- src/StringExtension.js
+++ src/StringExtension.js
@@ -2,3 +2,17 @@
 String.prototype.hasVowels = function () {
   return /[aeiou]/gi.test(this);
 };
+
+String.prototype.toUpper = function () {
+  return validData(this) ? this.replace(/[a-z]/g, upperCase) : '';
+};
+
+function upperCase(word) {
+  return String.fromCharCode(word.charCodeAt() - 32);
+}
+
+function validData(word){
+  if(word){
+    return true;
+  }
+}
