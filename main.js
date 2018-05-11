4a3cc692f903f1560273a0cc2d5960c24e6241aa
Jolaade Adewale <jolaade.adewale@andela.com>
2018-05-11 16:34:12 +0100

    add git ignore

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
 var http = require('http');
 var express = require('express');
+var open = require("open");
 var httpApp = express();
 httpApp.use(express.static(__dirname + '/'));
 httpApp.get('/', function (req,res) {
     res.sendFile(__dirname+ '/' + 'jasmine/SpecRunner.html');
   });
-var webserver = http.createServer(httpApp).listen(8084);
+var webserver = http.createServer(httpApp).listen(8084, function (success) {
+  open("http://localhost:8084");
+});
